fcf4bc0cb83e01f2fd400556cfa0df2a63eb22c1
NityaNanda Adhikary <nnadkbdhupguri@gmail.com>
2024-05-26 23:48:18 +0530

    emergency commit

--- app.py
+++ app.py
@@ -48,25 +48,37 @@ def attendance():
     return render_template('index.html', selected_date=selected_date, attendance_data=updated_attendance_data)
 
 # Function to calculate and retrieve average attendance
-def get_average_attendance():
+def get_average_attendance(course, semester, subject):   
     conn = sqlite3.connect("attendance.db")
     cursor = conn.cursor()
 
     query = """
-    SELECT name, COUNT(*) as total_days,
-           ROUND((COUNT(*) / 30.0) * 100, 2) as average_attendance
+    SELECT name, 
+           COUNT(*) as total_days,
+           ROUND((COUNT(*) * 100.0) / (SELECT COUNT(*) FROM attendance WHERE course = ? AND semester = ? AND subject = ?), 2) as average_attendance
     FROM attendance
+    WHERE course = ? AND semester = ? AND subject = ?
     GROUP BY name;
     """
-    cursor.execute(query)
+    cursor.execute(query, (course, semester, subject, course, semester, subject))
     data = cursor.fetchall()
     conn.close()
     return data
 
 # Route to handle the request for average attendance
-@app.route('/average_attendance')
+@app.route('/overall',methods=['GET'])
+def index():
+    return render_template('overall_attendance.html', selected_date='', no_data=False)
+
+
+@app.route('/overall_attendance',methods=['POST'])
 def average_attendance():
-    avg_attendance_data = get_average_attendance()
+    course = request.form.get('course')
+    semester = request.form.get('semester')
+    subject = request.form.get('subject')
+
+    avg_attendance_data = get_average_attendance(course, semester, subject)
+    # avg_attendance_data = get_average_attendance()
     return render_template('average_attendance.html', avg_attendance_data=avg_attendance_data)
 
 if __name__ == '__main__':
